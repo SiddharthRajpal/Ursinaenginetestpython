a749b899008427626f1da3a61221c33fec522b2f
SiddharthRajpal <77538244+SiddharthRajpal@users.noreply.github.com>
2021-01-19 23:57:10 +0530

    Add files via upload

---
+++ ursina1.py
@@ -0,0 +1,25 @@
+from ursina import *
+screen= Ursina()
+test1=Entity(model="StarSparrow03", rotation=(170,38,48), texture="StarSparrow_Emissive", postion=(0,100,0), scale=(1,1,1))
+def update():
+    if held_keys['w']:
+        test1.y+=5*time.dt
+    elif held_keys['s']:
+        test1.y-= 5*time.dt
+    elif held_keys['a']:
+        test1.x-= 5*time.dt
+    elif held_keys['d']:
+        test1.x+= 5*time.dt
+    elif held_keys['i']:
+        test1.z-=5*time.dt
+    elif held_keys['k']:
+        test1.z+=5*time.dt
+    elif held_keys['right arrow']:
+        test1.rotation_y-=30*time.dt
+    elif held_keys['left arrow']:
+        test1.rotation_y+=30*time.dt
+    elif held_keys['up arrow']:
+        test1.rotation_x-=30*time.dt
+    elif held_keys['down arrow']:
+        test1.rotation_x+=30*time.dt
+screen.run()W
